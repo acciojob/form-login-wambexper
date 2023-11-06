899673d13a3014bb126e6834cb4386f4fb1cb2b4
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-06 16:30:58 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the form elements by their name attributes
+    const firstName = document.forms["form1"]["fname"].value;
+    const lastName = document.forms["form1"]["lname"].value;
+    
+    // Format the alert message to match the expected format
+    alert(firstName + ' ' + lastName);
 
+    // Prevent the form from submitting and refreshing the page
+    return false;
 }
